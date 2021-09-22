2d25c7bd8138dac30cc0cfbb1b4d07acdf373b69
miraj <mohd@infino.co>
2021-09-22 10:42:30 +0530

    buil inint

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,56 +1,84 @@
-import React from 'react'
-import Image from 'next/dist/client/image';
-import {ChatAlt2Icon, PaperClipIcon,PlusIcon   } from '@heroicons/react/outline';
-function CardItem({data}) {
+import React from "react";
+import Image from "next/dist/client/image";
+import {
+  ChevronDownIcon,
+  PlusIcon,
+  DotsVerticalIcon,
+  ChatAlt2Icon,
+  PaperClipIcon,
+} from "@heroicons/react/outline";
+import { Draggable } from "react-beautiful-dnd";
 
-    return (
-        <>
-        {data ? 
-        <div className="bg-white rounded-md p-3 mt-3">
-            <label 
-                className={`bg-gradient-to-r from-blue-500 to-blue-200 px-2 py-1 text-white text-sm 
-                    ${data.priority === 0 ? 'from-blue-400 to-blue-500' :
-                        data.priority === 1 ? 'from-green-500 to-blue-500' :
-                        'from-red-500 to-red-200'
-                    }
-                    
-                `}>
-                {                
-                
-                    data.priority === 0 ? "Low Prioprity" : data.priority === 1 ?
-                    "Medius Priority" : "High Priority"
-                }
-                 
-            </label>
-            <h5 className="text-md my-3 text-lg leading-6">Company website redesign</h5>
-            <div className="flex justify-between">
-                <div className="flex space-x-2 items-center">
-                <span className="flex space-x-2 items-center">
-                    <ChatAlt2Icon className="w-4 h-4 text-gray-500"/>
-                    <span>1</span>
-                </span>
-                <span className="flex space-x-2 items-center">
-                    <PaperClipIcon className="w-4 h-4 text-gray-500"/>
-                    <span>1</span>
-                </span>
-                </div>
-                <ul className="flex space-x-3">
-                
-                <li>
-                    <Image src="https://randomuser.me/api/portraits/men/77.jpg" width="36" height="36" objectFit="cover" className="rounded-full"/>
-                </li>
-                <li>
-                    <button className="border border-dashed flex items-center w-9 h-9 border-gray-500 justify-center rounded-full">
-                    <PlusIcon className="w-5 h-5 text-gray-500"/>
-                    </button>
-                </li>
-
-                </ul>
+function CardItem({ data, index }) {
+  return (
+    <Draggable index={index} draggableId={data.id.toString()}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="bg-white rounded-md p-3 m-3 mt-0 last:mb-0"
+        >
+          <label
+            className={`bg-gradient-to-r
+              px-2 py-1 rounded text-white text-sm
+              ${
+                data.priority === 0
+                  ? "from-blue-600 to-blue-400"
+                  : data.priority === 1
+                  ? "from-green-600 to-green-400"
+                  : "from-red-600 to-red-400"
+              }
+              `}
+          >
+            {data.priority === 0
+              ? "Low Priority"
+              : data.priority === 1
+              ? "Medium Priority"
+              : "High Priority"}
+          </label>
+          <h5 className="text-md my-3 text-lg leading-6">{data.title}</h5>
+          <div className="flex justify-between">
+            <div className="flex space-x-2 items-center">
+              <span className="flex space-x-1 items-center">
+                <ChatAlt2Icon className="w-4 h-4 text-gray-500" />
+                <span>{data.chat}</span>
+              </span>
+              <span className="flex space-x-1 items-center">
+                <PaperClipIcon className="w-4 h-4 text-gray-500" />
+                <span>{data.attachment}</span>
+              </span>
             </div>
+
+            <ul className="flex space-x-3">
+              {data.assignees.map((ass, index) => {
+                return (
+                  <li key={index}>
+                    <Image
+                      src={ass.avt}
+                      width="36"
+                      height="36"
+                      objectFit="cover"
+                      className=" rounded-full "
+                    />
+                  </li>
+                );
+              })}
+              <li>
+                <button
+                  className="border border-dashed flex items-center w-9 h-9 border-gray-500 justify-center
+                    rounded-full"
+                >
+                  <PlusIcon className="w-5 h-5 text-gray-500" />
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-        : false}
-        </>
-    )
+        
+      )}
+    </Draggable>
+  );
 }
 
-export default CardItem
+export default CardItem;
